783cfbb1cc551e1180bd6f83560b1c7e1b454702
thederpykrafter <thederpykrafter@gmail.com>
2025-01-14 14:13:37 -0500

    idk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+  import NavBar from './NavBar.svelte';
 </script>
+ 
+<NavBar />
 
 {@render children()}
---
+++ src/routes/NavBar.svelte
@@ -0,0 +1,5 @@
+<nav class='bg-gray-800'>
+  <a class='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+    <span>Home</span>
+  </a>
+</nav>
